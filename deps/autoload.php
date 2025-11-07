7cc982311b0d6a1a05533cce369c002c2c018eb5
Jan Potužník <jan.potuznik@gmail.com>
2025-11-07 11:23:10 +0100

    PES-2905: deps update

--- deps/autoload.php
+++ deps/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc565d9f804ece6eb62e4821ef4e6957c::getLoader();
+return ComposerAutoloaderInitf47a322e83e83fe5711594ffed3fd52a::getLoader();
--- deps/composer/autoload_static.php
+++ deps/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc565d9f804ece6eb62e4821ef4e6957c
+class ComposerStaticInitf47a322e83e83fe5711594ffed3fd52a
 {
     public static $files = array (
         'packeteryd507e002f7fce7f0c6dbf1f22edcb902' => __DIR__ . '/..' . '/tracy/tracy/src/Tracy/functions.php',
@@ -302,7 +302,7 @@ class ComposerStaticInitc565d9f804ece6eb62e4821ef4e6957c
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->classMap = ComposerStaticInitc565d9f804ece6eb62e4821ef4e6957c::$classMap;
+            $loader->classMap = ComposerStaticInitf47a322e83e83fe5711594ffed3fd52a::$classMap;
 
         }, null, ClassLoader::class);
     }
